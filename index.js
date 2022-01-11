a6cae0828d08b8733eb3cf495bed10b364356783
Kimberly Hayes <kshayes78@gmail.com>
2022-01-11 13:08:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+
+//function distanceFromHqInBlocks(blocks){ 
+//let distance = Math.abs(blocks -42);
+//return (distance)
+//}
+
+function distanceFromHqInBlocks(block){
+    const hq=42;
+    if (block>=hq){
+        return block-hq;
+    } else {
+        return hq-block;
+    }
+}
+
+
+function distanceFromHqInFeet(block){
+    return distanceFromHqInBlocks(block) * 264
+}
+
+function distanceTravelledInFeet(start, dest) {
+    return Math.abs(start - dest) * 264
+}
+
+function calculatesFarePrice(start, dest){
+    const distance = distanceTravelledInFeet(start, dest)
+    if (distance < 400){
+        return 0
+    }else if (distance <2000){
+    return .02 * (distance - 400)
+}   
+else if (distance > 2000 && distance < 2500) {
+    return 25
+} else if (distance > 2500){
+    return 'cannot travel that far'
+}
+}
+
+
